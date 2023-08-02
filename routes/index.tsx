3f589cd3ec57366ffe0da402581adbe12c0c098a
Raymond Gao <raygao06@gmail.com>
2023-08-02 22:47:19 +0200

    transparent uxig picture
    created uxig transparent picture

---
+++ routes/index.tsx
@@ -0,0 +1,32 @@
+import { Head } from "$fresh/runtime.ts";
+import { useSignal } from "@preact/signals";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  const count = useSignal(3);
+  return (
+    <>
+      <Head>
+        <title>uxig</title>
+      </Head>
+      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <img
+            class="my-6"
+            src="/uxig.png"
+            width="128"
+            height="128"
+            alt="the fresh logo: a sliced lemon dripping with juice"
+          />
+          <h1 class="text-4xl font-bold">Welcome!</h1>
+          <h2>An interactive tools for User Experience Research</h2>
+          <p class="my-4">
+            Try updating this message in the
+            <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          </p>
+          <Counter count={count} />
+        </div>
+      </div>
+    </>
+  );
+}
