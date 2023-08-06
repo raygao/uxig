5dd1a4d9d026279c9924a2dba00e1767a8eb1ee8
Raymond Gao <raygao06@gmail.com>
2023-08-06 11:38:30 +0200

    using environment var for mongo connection
    using environment var for mongo connection

--- Data/MongoDB.tsx
+++ Data/MongoDB.tsx
@@ -1,26 +1,28 @@
 import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.31.2/mod.ts";
 
-//await client.connect("mongodb://ray:ray@127.0.0.1:27017/?authMechanism=SCRAM-SHA-1&authSource=uxig");
+const mongoConnectionString = Deno.env.get("mongoConnectionString")!;
+console.log("mongo connection string is: "+ mongoConnectionString);
 
 // Connecting to a Mongo Atlas Database, tls should be false for the local system
 export async function getMongoConnection() {
   const client = new MongoClient();
-  await client.connect({
-    db: "uxig",
-    tls: false,
-    servers: [
-      {
-        host: "127.0.0.1",
-        port: 27017,
-      },
-    ],
-    credential: {
-      username: "ray",
-      password: "ray",
-      db: "uxig",
-      mechanism: "SCRAM-SHA-1",
-    },
-  });
+  await client.connect(mongoConnectionString);
+  // await client.connect({
+  //   db: "uxig",
+  //   tls: false,
+  //   servers: [
+  //     {
+  //       host: "127.0.0.1",
+  //       port: 27017,
+  //     },
+  //   ],
+  //   credential: {
+  //     username: "ray",
+  //     password: "ray",
+  //     db: "uxig",
+  //     mechanism: "SCRAM-SHA-1",
+  //   },
+  // });
   return client;
 }
 
